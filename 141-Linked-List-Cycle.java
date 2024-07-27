72e8897fb32776ff4cb8e40d10b08ec7b3db2369
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-07-27 23:03:45 +0200

    [141] [Time Beats: 100.00%] [Memory Beats: 5.87%] - LeetPush

--- 141-Linked-List-Cycle.java
+++ 141-Linked-List-Cycle.java
@@ -1,31 +1,33 @@
 /**
  * Definition for singly-linked list.
  * class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) {
- * val = x;
- * next = null;
- * }
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
  * }
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // use Floyd warshall algorithm--> in a nutshell I will have slow and fast
-        // pointer and if they meet means
-        // that I have a loop
+        //check if there's any null values
+        if(head == null || head.next == null){
+            return false;
+        }
+        //create two pointers 
         ListNode slow = head;
-        ListNode fast = head;
-        // check if there's any null values, I will return false
-        // as long as it's a cycle means there's no null values
-        while (slow != null && fast != null && fast.next != null) {
+        ListNode fast = head.next;
+
+        //if the pointers don't meet means keep checking 
+        //otherwise return true because the pointers meet
+        while(slow!=fast){
+            if(fast == null || fast.next == null){
+                return false;
+            }
             slow = slow.next;
             fast = fast.next.next;
-            // if the two pointers meet, I found a loop
-            if (slow == fast) {
-                return true;
-            }
         }
-        return false;
+        return true;
     }
 }
