7c615f7ec2110f2a6ffc9fd97e933500bedb75ae
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-07-31 03:10:47 +0200

    [21] [Time Beats: 100.00%] [Memory Beats: 53.05%] - LeetPush

--- 21-Merge-Two-Sorted-Lists.java
+++ 21-Merge-Two-Sorted-Lists.java
@@ -1,29 +1,34 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null) return null;
+        ListNode returnNode = new ListNode(Integer.MIN_VALUE);
+        ListNode headNode = returnNode;
 
-        
-        if(list1 != null && list2 != null){
-            if(list1.val < list2.val){
-                list1.next = mergeTwoLists(list1.next, list2);
-                return list1;
-            }else{
-                list2.next = mergeTwoLists(list1, list2.next);
-                return list2;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                returnNode.next = list1;
+                list1 = list1.next;
+            } else {
+                returnNode.next = list2;
+                list2 = list2.next;
             }
-    }
-     if(list1==null)
-            return list2;
-        return list1;      
+            returnNode = returnNode.next;
+        }
+        if (list1 == null) {
+            returnNode.next = list2;
+
+        } else if (list2 == null) {
+            returnNode.next = list1;
+        }
+        return headNode.next;
     }
 }
